0bfd4f6343df4535197ac368b194329520a5fb43
alexyyaskiewicz280 <alexyyaskiewicz280@gmail.com>
2025-03-24 09:45:03 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1 = 5;
+    int number2 = 3;
+    double result = (number1 + number2) * 2.0 / 3.0; // Simplified version: sum + divide by three
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
