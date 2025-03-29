6598db854a47dac8ae63c7e6fce1fbd21ac318ba
alexyyaskiewicz280 <alexyyaskiewicz280@gmail.com>
2025-03-29 19:56:14 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter an integer: ";
+    cin >> n;
+    
+    if (n % 2 == 0) {
+        cout << n * 2 + 1;
+    } else {
+        cout << n * 2 - 1;
+    }
+    
+    return 0;
+}
