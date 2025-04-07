59473140784f1e8181737e71ef84720775498558
alexyyaskiewicz280 <alexyyaskiewicz280@gmail.com>
2025-04-07 16:25:11 +0000

    Add 101.cpp

---
+++ src/101.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1 = 5;
+    int number2 = 3;
+
+    cout << "The sum of " << number1 << " and " << number2 << " is " << (number1 + number2) << endl;
+    return 0;
+}
