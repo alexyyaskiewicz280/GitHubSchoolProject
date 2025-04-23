dbe3cd7019d1db5c602769511c9881bd6a971b96
alexyyaskiewicz280 <alexyyaskiewicz280@gmail.com>
2025-04-23 11:19:10 +0000

    Add 141.cpp

---
+++ src/141.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number" << endl;
+    } else {
+        cout << "Negative or zero" << endl;
+    }
+    return 0;
+}
